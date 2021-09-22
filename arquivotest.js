9d83064413ec0b8f2d8be4bd6e1ea0526aff48af
Julio Vernner <julioart12@hotmail.com>
2021-09-22 17:43:31 -0300

    Adicionando arquivos e commit

--- arquivotest.js
+++ arquivotest.js
@@ -1,1 +1,27 @@
-//nothing
+/*function createButton()
+{
+     var btn = document.createElement('BUTTON');
+     var lbl = document.createTextNode("CLICK ME");        
+     btn.appendChild(lbl);   
+     btn.onclick = function()
+     {
+        window.history.go(0);
+     }
+     document.body.appendChild(btn);    
+}*/
+
+
+function criarBotao() {
+    var btn = document.createElement('button');
+    var nomebtn = document.createTextNode('Me clica')
+
+    btn.appendChild(nomebtn);
+    document.body.appendChild(btn);
+
+    btn.onclick = function() {
+        alert('Clicado')
+        console.log('Apertou')
+    }
+}
+
+criarBotao();
